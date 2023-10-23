bc07d33470f1765fc10f1c0755f04bd0a58b1b0b
anaresgalla <anaresgalla@gmail.com>
2023-10-23 11:36:46 -0300

    Criando formularios para cadastro de novos galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,22 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end 
+
+  def new
+  end
+
+  def create
+    #É aqui dentro que vamos:
+    #1- Receber os dados enviados
+
+    #2- Criar um novo GALPÃO(nesse caso) no banco de dados
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :address,
+                                       :description, :cep, :area) #=> STRONG PARAMETERS
+    w = Warehouse.new(warehouse_params)
+    w.save
+
+    #3- Redirecionar para a TELA INICIAL(nesse caso)
+    #flash[:notice] = "Galpão cadastrado com sucesso!" --> uma das formas de escrever o flash, mas pode usar o notice ou o alert com o redirect direto, como abaixo:
+    redirect_to root_path, notice: 'Galpão cadastrado com sucesso!'
+  end 
 end 
---
+++ spec/system/user_register_warehouse_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um galpão' do 
+  it 'a partir da tela inicial' do
+    #Arrange --> não precisa de ter um galpão cadastrado previamente. pode ser o cadastro do 1º galpão
+
+    #Act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+
+    #Assert
+    expect(page).to have_field('Nome')
+    expect(page).to have_field('Descrição')
+    expect(page).to have_field('Código')
+    expect(page).to have_field('Endereço')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Área')
+  end 
+
+  it 'com sucesso' do
+    #Arrange --> não precisa de ter um galpão cadastrado previamente. Geralmente não tem arrange para formulários
+
+    #Act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Descrição', with: 'Galpão da área da zona portuária do Rio'
+    fill_in 'Código', with: 'RIO'
+    fill_in 'Endereço', with: 'Avenida do Museu do Amanhã, 1000'
+    fill_in 'Cidade', with: 'Rio de Janeiro'
+    fill_in 'CEP', with: '20100-000'
+    fill_in 'Área', with: '32000'
+    click_on 'Enviar'
+
+    #Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpão cadastrado com sucesso!' 
+    expect(page).to have_content 'Rio de Janeiro'
+    expect(page).to have_content 'RIO'
+    expect(page).to have_content '32000 m²'
+  end 
+end
