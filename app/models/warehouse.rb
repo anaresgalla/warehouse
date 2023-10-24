0853d7c2f2f032a1f360ad0b0d6ee4a76fb74070
anaresgalla <anaresgalla@gmail.com>
2023-10-24 19:08:29 -0300

    Criando Testes Unitários

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -4,4 +4,6 @@ class Warehouse < ApplicationRecord
   #não pode passar duas validações para o mesmo atributo ao mesmo tempo = na mesma linha. 
   # por exemplo: para validar o code pela presença e pelo nunero de caracteres, teria q validar cada um em uma linha de código separada 
   #ex: validades :code, presence: true  E  validades :code, length {is: 3}
+  validates :code, uniqueness: true
+  #a validação uniqueness analisa dados cadastrados no sql, então os dados devem estar lá
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
- 
+  describe '#valid?' do #(tem o # pq é teste de instâncias da classe)
+    context 'presence' do #agrega todos os testes unitários do mesmo tipo
+      it 'false when name is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Endereço', cep: '25000-000', 
+                                city: 'Rio', area: 1000, description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+      end 
+
+      it 'false when code is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: '', address: 'Endereço', cep: '25000-000', 
+                                  city: 'Rio', area: 1000, description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+
+        ############ essa linha substitui as linhas de act e assert -> expect(warehouse).not_to be_valid
+        #pq executa o valid? e espera que ele seja falso
+      end 
+
+      it 'false when address is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: '', cep: '25000-000', 
+                                  city: 'Rio', area: 1000, description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+        ####### posso refatorar o código excluindo a linha act, deixando ela junto com a parte do assert:
+        #expect(warehouse.valid?).to eq false
+      end
+
+      it 'false when cep is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '', 
+                                  city: 'Rio', area: 1000, description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+      end 
+
+      it 'false when city is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '25000-000', 
+                                  city: '', area: 1000, description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+      end 
+
+      it 'false when area is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '25000-000', 
+                                  city: 'Rio', area: nil, description: 'Alguma descrição')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+      end 
+
+      it 'false when description is empty' do
+        #Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '25000-000', 
+                                  city: 'Rio', area: 1000, description: '')
+        #Act
+        result = warehouse.valid?
+        #Assert
+        expect(result).to eq false 
+      end 
+    end 
+
+      it 'false when code is already in use' do
+        #Arrange: Criar 2 galpões, Repetir o código
+        first_warehouse = Warehouse.create(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '25000-000', 
+                                           city: 'Rio', area: 1000, description: 'Alguma descrição')
+        second_warehouse = Warehouse.new(name: 'Niteroi', code: 'RIO', address: 'Avenida', cep: '27000-000', 
+                                            city: 'Niteroi', area: 2000, description: 'Outra descrição')
+        
+        #Act: Perguntar para o 2º galpão se ele é valido
+        result = second_warehouse.valid?
+        
+        #Assert: Espero que seja false(não pode ser válido)
+        expect(result).to eq false 
+      end
+  end     
 end
