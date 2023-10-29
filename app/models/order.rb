b475cadd1f33bdd83f19a4aaa15962f9d5871770
anaresgalla <anaresgalla@gmail.com>
2023-10-29 10:36:57 -0300

    Faz associações em pedidos

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ApplicationRecord
   belongs_to :supplier
   belongs_to :user
   enum status: { pending: 0, delivered: 5, canceled: 9 }
+  has_many :order_items
+  has_many :product_models, through: :order_items
 
   validates :code, :estimated_delivery_date, presence: true 
   validate :estimated_delivery_date_is_future
--- spec/system/orders/user_view_own_orders_spec.rb
+++ spec/system/orders/user_view_own_orders_spec.rb
@@ -85,4 +85,34 @@ describe 'Usuário vê seus próprios pedidos' do
     expect(current_path).to eq root_path
     expect(page).to have_content 'Você não possui acesso a este pedido.'
   end
+
+  it 'e vê itens do pedido' do
+    #Arrange
+    supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '123456',
+                                full_address: 'Avenida dos Coelhos, 50', city: 'Manaus', state: 'AM', 
+                                email: 'contato@acme.com.br')
+    product_a = ProductModel.create!(name: 'Produto A', weight: 15, width: 10, height: 20,
+                                     depth: 30, supplier: supplier, sku: '156GT')
+    product_b = ProductModel.create!(name: 'Produto B', weight: 15, width: 10, height: 20,
+                                     depth: 30, supplier: supplier, sku: '352KL')
+    product_c = ProductModel.create!(name: 'Produto C', weight: 15, width: 10, height: 20,
+                                     depth: 30, supplier: supplier, sku: '785PA')
+    user = User.create!(name: 'Leandro', email: 'leandro@email.com', password: 'password')
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
+                                  address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                                  description: 'Galpão destinado para cargas internacionais')
+    order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, 
+                          estimated_delivery_date: 1.day.from_now)
+    OrderItem.create!(product_model: product_a, order: order, quantity: 19)
+    OrderItem.create!(product_model: product_b, order: order, quantity: 12)
+    #Act
+    login_as(user)
+    visit root_path
+    click_on 'Meus Pedidos'
+    click_on order.code
+    #Assert
+    expect(page).to have_content 'Itens do Pedido'
+    expect(page).to have_content '19 X Produto A'
+    expect(page).to have_content '12 X Produto B'
+  end
 end
