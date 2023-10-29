fad2340544f65a074083b00cbf3b6065bc4bada9
anaresgalla <anaresgalla@gmail.com>
2023-10-29 12:55:35 -0300

    Faz a entrada de produtos no estoque

--- spec/system/orders/user_update_order_status_spec.rb
+++ spec/system/orders/user_update_order_status_spec.rb
@@ -10,8 +10,11 @@ describe 'Usuário informa novo status de pedido' do
     supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '123456',
                                 full_address: 'Avenida dos Coelhos, 50', city: 'Manaus', state: 'AM', 
                                 email: 'contato@acme.com.br')
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5, height: 100, width: 70,
+                                   depth: 75, sku: 'CAD-13XY')
     order = Order.create!(user: leandro, warehouse: warehouse, supplier: supplier, 
                                 estimated_delivery_date: 1.day.from_now, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
     #Act
     login_as leandro
     visit root_path
@@ -22,6 +25,11 @@ describe 'Usuário informa novo status de pedido' do
     expect(current_path).to eq order_path(order.id)
     expect(page).to have_content 'Situação do Pedido: Entregue'
     expect(page).not_to have_button 'Marcar como CANCELADO'
+    expect(page).not_to have_button 'Marcar como ENTREGUE'
+    expect(StockProduct.count).to eq 5
+    estoque = StockProduct.where(product_model: product, warehouse: warehouse).count
+    expect(estoque).to eq 5
+
   end
   
   it 'e pedido foi cancelado' do
@@ -33,8 +41,12 @@ describe 'Usuário informa novo status de pedido' do
     supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '123456',
                                 full_address: 'Avenida dos Coelhos, 50', city: 'Manaus', state: 'AM', 
                                 email: 'contato@acme.com.br')
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5, height: 100, width: 70,
+                                   depth: 75, sku: 'CAD-13XY')
     order = Order.create!(user: leandro, warehouse: warehouse, supplier: supplier, 
-                                estimated_delivery_date: 1.day.from_now, status: :pending)
+                          estimated_delivery_date: 1.day.from_now, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
+    
     #Act
     login_as leandro
     visit root_path
@@ -44,5 +56,6 @@ describe 'Usuário informa novo status de pedido' do
     #Assert
     expect(current_path).to eq order_path(order.id)
     expect(page).to have_content 'Situação do Pedido: Cancelado'
+    expect(StockProduct.count).to eq 0
   end
 end
